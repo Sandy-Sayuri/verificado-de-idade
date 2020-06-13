bdde20b793d8359d82361f912068a1306f74b2ef
Sandy-sayuri <sandysayuri200@gmail.com>
2020-06-13 12:44:15 -0300

    resolução

--- script.js
+++ script.js
@@ -16,42 +16,54 @@ function verificar(){
            gênero="Homen"
            if(idade>=0 && idade<3){
                //bebe
+               img.setAttribute('src','bebe H.jpg')
            }
            else if(idade>=3 && idade<10){
                //criança
+               img.setAttribute('src','criançaH.jpg')
            }
            else if(idade>=10 && idade<21){
                //adolecente
+               img.setAttribute('src','jovemH.jpg')
            }
            else if(idade>=21 && idade<50){
                //adulto
+               img.setAttribute('src','adultoH.jpg')
            }
            else if(idade>=50 && idade<200){
                //velho
+               img.setAttribute('src','velhoH.jpg')
            }
            else{
                //vampiro
+               img.setAttribute('src','vampiroH.jpg')
            }
        }
        else if(fsex[1].checked){
         gênero='Mulher'
         if(idade>=0 && idade<3){
             //bebe
+            img.setAttribute('src','bebeM.jpg')
         }
         else if(idade>=3 && idade<10){
             //criança
+            img.setAttribute('src','criançaM.jpg')
         }
         else if(idade>=10 && idade<21){
             //adolecente
+            img.setAttribute('src','jovemM.jpg')
         }
         else if(idade>=21 && idade<50){
             //adulto
+            img.setAttribute('src','adultoM.jpg')
         }
         else if(idade>=50 && idade<200){
             //velho
+            img.setAttribute('src','velhoM.jpg')
         }
         else{
             //vampiro
+            img.setAttribute('src','vampiroM.jpg')
         }
      }
        res.style.textAlign='center'
